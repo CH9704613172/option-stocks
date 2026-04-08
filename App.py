7fde3be8139ef5a12f845d20cbfec187e5886824
CH9704613172 <chandolubhargav396@gmail.com>
2026-04-08 22:53:19 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ warnings.filterwarnings("ignore")
 import numpy as np
 import pandas as pd
 import yfinance as yf
-import pandas_ta as ta
+import pandas_ta_classic as ta
 import streamlit as st
 
 # ══════════════════════════════════════════════════════════════════
